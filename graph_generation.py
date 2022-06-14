da9b00383ac96fbefab7ed035b31d610f9d5f720
atarte <tarte.antoine@gmail.com>
2022-06-14 23:01:51 +0200

    check and fix connected graph

--- graph_generation.py
+++ graph_generation.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.linalg import matrix_power
 import random as rand
 
 def Init_Matrix(nb_Vertex):
@@ -15,6 +16,34 @@ def Init_Matrix(nb_Vertex):
     return matrix
 
 
+def Fix_Matrix_Not_Connected(matrix):
+    '''
+    Returns a connected matrix from a non connected matrix
+    '''
+    print('fix matrix')
+
+    nb_Vertex = len(matrix)
+
+    adjacency_matrix = np.asarray(matrix)
+    np.fill_diagonal(adjacency_matrix, 1)
+
+    # en theorie il faut mettre les poid a 1
+
+    adjacency_matrix = matrix_power(adjacency_matrix, nb_Vertex)
+
+    rand_vertex = rand.randint(0, nb_Vertex -1)
+
+    neighbour_to_connect = [ index for index, value in enumerate(matrix[rand_vertex]) if value == 0 and index != rand_vertex ]
+    new_neighbour = rand.choice(neighbour_to_connect)
+
+    weight = 1
+
+    matrix[rand_vertex][new_neighbour] = weight
+    matrix[new_neighbour][rand_vertex] = weight
+
+    return matrix
+
+
 def Check_Matrix_Connected(matrix):
     '''
     Returns a boolean to check if an adjacency matrix matches a connected graph
@@ -22,7 +51,26 @@ def Check_Matrix_Connected(matrix):
     True : the graph is connected
     Fasle : the graph is not connected
     '''
-    return true
+    print('check matrix')
+
+    nb_Vertex = len(matrix)
+
+    adjacency_matrix = np.asarray(matrix)
+    np.fill_diagonal(adjacency_matrix, 1)
+
+    # for i in range(nb_Vertex):
+    #     for j in range(nb_Vertex):
+    #         if adjacency_matrix[i][j] != 0:
+    #             adjacency_matrix[i][j] = 1
+
+    adjacency_matrix = matrix_power(adjacency_matrix, nb_Vertex)
+    # print(adjacency_matrix)
+
+    for line in adjacency_matrix:
+        if not 0 in line:
+            return True
+
+    return False
 
 
 def Get_Neighbour_List(nb_Vertex):
@@ -92,31 +140,34 @@ def Get_Adjacency_Matrix(nb_Vertex):
         vertex_neighbour_list = rand.sample(neighbour_allow, vertex_nb_neighbour)
 
         for vertex_neighbour in vertex_neighbour_list:
+            weight = 1
+            # weight = rand.randint(1, 10)
 
-            matrix[vertex_index][vertex_neighbour] = 1 # le poid plutard
-            matrix[vertex_neighbour][vertex_index] = 1 # le poid plutard
+            matrix[vertex_index][vertex_neighbour] = weight
+            matrix[vertex_neighbour][vertex_index] = weight
+
+    while not Check_Matrix_Connected(matrix):
+        matrix = Fix_Matrix_Not_Connected(matrix)
     
     return matrix
     
-
-def test():
-    print('cool')
-
 
 if __name__ == "__main__":
     nb_Vertex = 6
 
     matrix = Get_Adjacency_Matrix(nb_Vertex)
-
+    # matrix = [
+    #     [0, 2, 5, 0, 0],
+    #     [2, 0, 2, 0, 0],
+    #     [2, 4, 0, 0, 0],
+    #     [0, 0, 0, 0, 5],
+    #     [0, 0, 0, 3, 0],
+    # ]
+    
     print('matrix : ')
     for m in matrix:
         print(m)
 
-
-
-
-
-
-    print('end')
-
-
+    # print('')
+    # IsMatrixConnected = Check_Matrix_Connected(matrix)
+    # print('Is the matrix connected : ', IsMatrixConnected)
