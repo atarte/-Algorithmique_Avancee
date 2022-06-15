013e521f05a692392ef840bf29e4d4ed315f054c
C-atarina <catarina.silvaroriz@viacesi.fr>
2022-06-15 15:38:59 +0200

    graph representation

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Algorithmique Avancée
 
 pip3 install --upgrade jupyter matplotlib numpy pulp voila
+pip install networkx
--- graph_generation.py
+++ graph_generation.py
@@ -1,6 +1,10 @@
+from matplotlib import style
 import numpy as np
 from numpy.linalg import matrix_power
 import random as rand
+import matplotlib.pyplot as plt
+import networkx as nx
+
 
 def Init_Matrix(nb_vertex):
     '''
@@ -31,9 +35,10 @@ def Fix_Matrix_Not_Connected(matrix):
 
     adjacency_matrix = matrix_power(adjacency_matrix, nb_vertex)
 
-    rand_vertex = rand.randint(0, nb_vertex -1)
+    rand_vertex = rand.randint(0, nb_vertex - 1)
 
-    neighbour_to_connect = [ index for index, value in enumerate(matrix[rand_vertex]) if value == 0 and index != rand_vertex ]
+    neighbour_to_connect = [index for index, value in enumerate(
+        matrix[rand_vertex]) if value == 0 and index != rand_vertex]
     new_neighbour = rand.choice(neighbour_to_connect)
 
     # weight = 1
@@ -97,7 +102,7 @@ def Get_Neighbour_List(nb_vertex):
             ])
 
         order_total = np.sum(neighbour_list, axis=0)[1]
-    
+
     return neighbour_list
 
 
@@ -107,7 +112,8 @@ def Get_Adjacency_Matrix(nb_vertex):
     '''
     matrix = Init_Matrix(nb_vertex)
     neighbour_list = Get_Neighbour_List(nb_vertex)
-    neighbour_list_sorted = sorted(neighbour_list, key=lambda x:x[1], reverse=True)
+    neighbour_list_sorted = sorted(
+        neighbour_list, key=lambda x: x[1], reverse=True)
 
     for vertex in neighbour_list_sorted:
 
@@ -132,11 +138,12 @@ def Get_Adjacency_Matrix(nb_vertex):
 
         if vertex_nb_neighbour <= 0:
             continue
-        
+
         if vertex_nb_neighbour > len(neighbour_allow):
             vertex_nb_neighbour = len(neighbour_allow)
 
-        vertex_neighbour_list = rand.sample(neighbour_allow, vertex_nb_neighbour)
+        vertex_neighbour_list = rand.sample(
+            neighbour_allow, vertex_nb_neighbour)
 
         for vertex_neighbour in vertex_neighbour_list:
             # weight = 1
@@ -147,25 +154,87 @@ def Get_Adjacency_Matrix(nb_vertex):
 
     while not Check_Matrix_Connected(matrix):
         matrix = Fix_Matrix_Not_Connected(matrix)
-    
+
     return matrix
-    
+
 
 if __name__ == "__main__":
     nb_vertex = 6
 
     matrix = Get_Adjacency_Matrix(nb_vertex)
     # matrix = [
-    #     [0, 2, 5, 0, 0],
-    #     [2, 0, 2, 0, 0],
-    #     [2, 4, 0, 0, 0],
-    #     [0, 0, 0, 0, 5],
-    #     [0, 0, 0, 3, 0],
+    #     [0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #     [5, 0, 2, 8, 2, 8, 7, 10, 2, 0, 2, 2, 8, 7, 0],
+    #     [0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0],
+    #     [0, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #     [0, 2, 0, 0, 0, 0, 0, 0, 0, 8, 0, 0, 9, 0, 7],
+    #     [0, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #     [0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #     [0, 10, 0, 0, 0, 0, 0, 0, 0, 7, 9, 0, 4, 0, 4],
+    #     [0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #     [0, 0, 0, 0, 8, 0, 0, 7, 0, 0, 0, 0, 1, 0, 1],
+    #     [0, 2, 0, 0, 0, 0, 0, 9, 0, 0, 0, 0, 4, 0, 0],
+    #     [0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #     [0, 8, 5, 0, 9, 0, 0, 4, 0, 1, 4, 0, 0, 0, 1],
+    #     [0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #     [0, 0, 0, 0, 7, 0, 0, 4, 0, 1, 0, 0, 1, 0, 0]
     # ]
-    
+
     print('matrix : ')
     for m in matrix:
         print(m)
+
+    G = nx.Graph()
+
+    for i in range(nb_vertex):
+        G.add_node(i, label=i, col='pink')
+
+    for i in range(nb_vertex):
+        for neighboor in range(i):
+            for weight in range(matrix[i][neighboor]):
+                if weight+1 > 0:
+                    G.add_edge(i, neighboor, weight=weight+1, styl='solid')
+
+    liste = list(G.nodes(data='col'))
+    colorNodes = {}
+    for noeud in liste:
+        colorNodes[noeud[0]] = noeud[1]
+    colorNodes
+
+    colorList = [colorNodes[node] for node in colorNodes]
+    colorList
+
+    liste = list(G.nodes(data='label'))
+    labels_nodes = {}
+    for noeud in liste:
+        labels_nodes[noeud[0]] = noeud[1]
+    labels_nodes
+
+    labels_edges = {}
+    labels_edges = {edge: G.edges[edge]['weight'] for edge in G.edges}
+    #labels_edges = {edge:'' for edge in G.edges}
+    labels_edges
+
+    # positions for all nodes
+    pos = nx.spring_layout(G)
+
+    # nodes
+    nx.draw_networkx_nodes(G, pos, node_size=700,
+                           node_color=colorList, alpha=0.9)
+
+    # labels
+    nx.draw_networkx_labels(G, pos, labels=labels_nodes,
+                            font_size=20,
+                            font_color='black',
+                            font_family='sans-serif')
+
+    # edges
+    nx.draw_networkx_edges(G, pos, width=1)
+    nx.draw_networkx_edge_labels(
+        G, pos, edge_labels=labels_edges, font_color='red')
+
+    plt.axis('off')
+    plt.show()
 
     # print('')
     # IsMatrixConnected = Check_Matrix_Connected(matrix)
