60c70f07b4d9806a99315aa9e2c61bce5701195f
atarte <tarte.antoine@gmail.com>
2022-06-23 21:39:53 +0200

    opti un max

--- tsp_ant.py
+++ tsp_ant.py
@@ -1,5 +1,5 @@
 import sys
-import random as rand
+# import random as rand
 from functools import lru_cache
 
 
@@ -9,47 +9,30 @@ def Get_Next_City(matrix, pheromone_matrix, path, cities_to_pass, current_city, 
     '''
     denominator_sum = 0
     probabilities_list = []
-    available_cities = [city for city in cities_to_pass if city not in path]
-
-    # if len(available_cities) == 0:
-    #     return
+    available_cities = tuple(city for city in cities_to_pass if city not in path)
 
     for city in available_cities:
-        intensity = pheromone_matrix[current_city][city]
-        visibility = 1 / matrix[current_city][city]
-        denominator_sum += pow(intensity, alpha) * pow(visibility, beta)
+        # intensity = pheromone_matrix[current_city][city]
+        # visibility = 1 / matrix[current_city][city]
+        # denominator_sum += pow(intensity, alpha) * pow(visibility, beta)
+        denominator_sum += pow(pheromone_matrix[current_city]
+                               [city], alpha) * pow(1 / matrix[current_city][city], beta)
 
-    if denominator_sum == 0:
-        print('city avai : ', available_cities)
-        print('current_city : ', current_city)
-        # print('city : ', city)
-        print('path : ', path)
-        print('pheromone_matrix : ', pheromone_matrix)
-        print('alpha : ', alpha)
-        print('beta : ', beta)
-
-        print()
-        for city in available_cities:
-            print('city : ', city)
-            intensity = pheromone_matrix[current_city][city]
-            print('intensity : ', intensity)
-
-            visibility = 1 / matrix[current_city][city]
-            print('visibility : ', visibility)
-
-            print('denominator_sum : ', denominator_sum)
-            denominator_sum += pow(intensity, alpha) * pow(visibility, beta)
-            print('denominator_sum : ', denominator_sum)
-        
 
     for city in available_cities:
-        intensity = pheromone_matrix[current_city][city]
-        visibility = 1 / matrix[current_city][city]
-        probability = (pow(intensity, alpha) * pow(visibility, beta)) / denominator_sum
+        # intensity = pheromone_matrix[current_city][city]
+        # visibility = 1 / matrix[current_city][city]
+        # probability = (pow(intensity, alpha) * pow(visibility, beta)) / denominator_sum
+        # probabilities_list.append([
+        #     city,
+        #     probability
+        # ])
         probabilities_list.append([
             city,
-            probability
+            (pow(pheromone_matrix[current_city][city], alpha)
+             * pow(1 / matrix[current_city][city], beta)) / denominator_sum
         ])
+
 
     probabilities_list_sorted = sorted(
         probabilities_list, key=lambda x: x[1], reverse=True)
@@ -65,15 +48,20 @@ def Update_Pheromone(pheromone_matrix, path, path_lenght, evaporation_factor, ph
 
     for i in range(len(pheromone_matrix)):
         for j in range(len(pheromone_matrix)):
-            pheromone_matrix[i][j] *= (1 - evaporation_factor)
+            # pheromone_matrix[i][j] *= (1 - evaporation_factor)
+            # if pheromone_matrix[i][j] <= 1.0e-250:
+            #     pheromone_matrix[i][j] = 1.0e-10
+            pheromone_matrix[i][j] = pheromone_matrix[i][j] * \
+                evaporation_factor if pheromone_matrix[i][j] <= 1.0e-250 else 1.0e-10
 
     for i in range(len(path) - 1):
-        pheromone_matrix[path[i]][path[i + 1]] = pheromone_to_apply
-        pheromone_matrix[path[i + 1]][path[i]] = pheromone_to_apply
+        pheromone_matrix[path[i]][path[i + 1]] += pheromone_to_apply
+        pheromone_matrix[path[i + 1]][path[i]] += pheromone_to_apply
 
     return pheromone_matrix
 
 
+@lru_cache(maxsize=512)
 def Get_Path_Lenght(graph, path):
     '''
     Returns the length of a path
@@ -86,33 +74,26 @@ def Get_Path_Lenght(graph, path):
     return path_lenght
 
 
-@lru_cache(maxsize=1024)
+@lru_cache(maxsize=512)
 def Ant_Tsp(graph, cities_to_pass, nb_iteration = 100, nb_ant = 10, alpha=1.0, beta=2.0, evaporation_factor=0.3, pheromone_spread=1.0):
     '''
     Returns the optimal path through the verteces <cities_to_pass> of the graph <graph> with the algoritm "Ant Colony Optimization"
     '''
     pheromone_matrix = [[1 for _ in graph] for _ in graph]
-    for i in range(len(pheromone_matrix)):
-        pheromone_matrix[i][i] = 0
     best_path = ()
-    path = []
     shortest_path = sys.maxsize
 
-    for i in range(nb_iteration):
-        # print('interation : ', i)
-        for ant in range(nb_ant):
-            # print('fourmis : ', ant)
+    for _ in range(nb_iteration):
+        for _ in range(nb_ant):
 
             path = []
-            current_city = rand.choice(cities_to_pass)
+            # current_city = rand.choice(cities_to_pass)
+            current_city = cities_to_pass[0]
             path.append(current_city)
 
             for _ in range(len(cities_to_pass) - 1):
 
                 next_city = Get_Next_City(graph, pheromone_matrix, path, cities_to_pass, current_city, alpha, beta)
-                # if next_city == None:
-                #     break
-
                 path.append(next_city)
                 current_city = next_city
 
